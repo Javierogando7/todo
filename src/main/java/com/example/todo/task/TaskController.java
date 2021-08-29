267c0bdfd006d876f6a1e7ac394f2c4f2afc0586
Javier Ogando <javierogando7@gmail.com>
2021-08-29 15:46:35 -0400

    Add CRUD to TaskController

--- src/main/java/com/example/todo/task/TaskController.java
+++ src/main/java/com/example/todo/task/TaskController.java
@@ -1,6 +1,14 @@
 package com.example.todo.task;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/task")
 public class TaskController {
 	private final ITaskService taskService;
-	
+
 	@Autowired
 	public TaskController(ITaskService taskService) {
 		this.taskService = taskService;
+	}
+
+	@GetMapping
+	public List<Task> getAllTasks() {
+		return taskService.getAllTasks();
+	}
+
+	@PostMapping
+	public void addNewTask(@RequestBody Task task) {
+		taskService.saveTask(task);
+	}
+
+	@PutMapping("{taskId}")
+	public void updateTask(@PathVariable("taskId") Long taskId, @RequestBody Task task) {
+		taskService.updateTask(taskId, task);
+	}
+
+	@DeleteMapping("{taskId}")
+	public void deleteTask(@PathVariable("taskId") Long taskId) {
+		taskService.deleteTask(taskId);
 	}
 }
